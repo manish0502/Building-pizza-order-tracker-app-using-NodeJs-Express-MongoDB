9ec2164d54fcf11c90ec2b1ea5a14e8fc51bd186
manish0502 <manishgoswami495@gmail.com>
2021-03-07 18:33:00 +0530

    1st-day

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app=express();
+const ejs= require('ejs');
+const expressLayouts=require('express-ejs-layouts');
+const path =require('path');
+const PORT = process.env.PORT || 3000;
+ 
+
+
+// @setting up routes
+
+app.get('/' ,(req, res)=>{
+    res.render('home')
+
+})
+
+// Setup Template Engine
+
+app.use(expressLayouts);
+app.set('views' ,path.join(__dirname,'/resources/views'))
+app.set('view engine' , 'ejs');
+
+
+//Listening server
+app.listen(PORT ,()=>{
+    console.log(`Server is running at port no: ${PORT}`);
+})
