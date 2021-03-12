2badca743eb44cc627e6074c470f2242588a718d
manish0502 <manishgoswami495@gmail.com>
2021-03-13 00:05:43 +0530

    changed cookie lifetime

--- server.js
+++ server.js
@@ -38,7 +38,8 @@ app.use(session({
     resave: false,
     store: mongoStore,
     saveUninitialized:false,
-    cookie:{ maxAge: 1000 * 60 * 60*24 } //24 hours
+    cookie:{ maxAge: 1000 * 15 }
+   // cookie:{ maxAge: 1000 * 60 * 60*24 } //24 hours
 
 
 }))
