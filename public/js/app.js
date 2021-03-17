007fd33d6dbf15d802e69143854906c8f3dcdb6f
manish0502 <manishgoswami495@gmail.com>
2021-03-17 13:35:21 +0530

    pizza-tracking app

--- public/js/app.js
+++ public/js/app.js
@@ -5346,8 +5346,8 @@ if (alertMsg) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/manish/codegyan/Realtime-pizza-order-tracker-app-using-NodeJs-Express-and-Mongo-DB/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/manish/codegyan/Realtime-pizza-order-tracker-app-using-NodeJs-Express-and-Mongo-DB/resources/scss/app.scss */"./resources/scss/app.scss");
+__webpack_require__(/*! /home/manish/github/Realtime-pizza-order-tracker-app-using-NodeJs-Express-and-Mongo-DB/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/manish/github/Realtime-pizza-order-tracker-app-using-NodeJs-Express-and-Mongo-DB/resources/scss/app.scss */"./resources/scss/app.scss");
 
 
 /***/ })
